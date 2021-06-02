2641f4b202a463085c9ef1b815a21064db3a17ef
rkecmfilenet <63664223+rkecmfilenet@users.noreply.github.com>
2021-06-01 21:00:02 -0400

    TerraformModules

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "region" {
+  default = "us-east-2"
+}
+
+variable "accesskey" {
+ default = "" 
+}
+
+variable "secretkey" {
+  default = ""
+}
+
+variable "imageid" {
+  default = "ami-0ba62214afa52bec7"
+}
+
+variable "key" {
+  default = "MyEC2Key"
+}
+
+variable "instancetype" {
+  default = "t2.micro"
+}
+
